d92453b0cecc3ff85a9580b9689ed53b2a17800c
Krøøs <kroos80@gmail.com>
2022-05-31 14:21:53 +0200

    création du formulaire des posts

---
+++ front/src/components/PostForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-form-input v-model="title" placeholder="Titre"></b-form-input>
+    <div class="mt-2">Value: {{ title }}</div>
+  </div>
+  <div>
+    <b-form-input v-model="content" placeholder="Entrez votre texte ici"></b-form-input>
+    <div class="mt-2">Value: {{ content }}</div>
+  </div>
+  <div>
+    <b-form-file v-model="file" class="mt-3" plain></b-form-file>
+    <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: '',
+        content: '',
+        file: null
+      }
+    }
+  }
+</script>
